b5bf85055c4dd746e8e901f2e8c0f8291c0724ec
fadhluibnu <f.ibnu.a1@gmail.com>
2022-03-31 13:47:59 +0700

    Upload Modul 7

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -17,7 +17,8 @@
                     <a class="nav-link {{ $title === 'Gallery' ? 'active' : '' }}" href="/gallery">Gallery</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link {{ $title === 'Contacts' ? 'active' : '' }}" href="/contacts">Contacts</a>
+                    <a class="nav-link {{ $title === 'Contacts' ? 'active' : '' }}"
+                        href="{{ route('contacts.create') }}">Contacts</a>
                 </li>
             </ul>
             <ul class="navbar-nav ms-auto">
